eb1ad368bcfadf8e515ad28829e08fcf4608bf0c
k-ibaraki <ibaraki@ncdc.co.jp>
2022-03-19 15:35:56 +0900

    hide sercret key

--- sampleProject/settings.py
+++ sampleProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hj%ohv24)br1f8%v&a#wv$i+uzx)6!0++x&$$y_(16=si9(_4$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
