5930e3adc6fb9c84b23052b1ffa5203f86bedc98
k-ibaraki <ibaraki@ncdc.co.jp>
2022-03-19 16:10:35 +0900

    dotenv導入

--- sampleProject/settings.py
+++ sampleProject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-sample-web-app.azurewebsites.net']
+ALLOWED_HOSTS = ['django-sample-web-app.azurewebsites.net',
+                 '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -38,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sample', # add
+    'sample',  # add
 ]
 
 MIDDLEWARE = [
