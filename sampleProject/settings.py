f37a425aecef16e56036ba69c1a8d5df01dc5464
k-ibaraki <ibaraki@ncdc.co.jp>
2022-04-11 13:19:33 +0900

    change env title

--- sampleProject/settings.py
+++ sampleProject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('HOST_NAME', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 
