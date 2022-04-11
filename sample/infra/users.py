d76e292308e00e2dbf4f7def0e089fcbd5b0b86b
k-ibaraki <ibaraki@ncdc.co.jp>
2022-04-11 19:04:58 +0900

    SQLAlchemyのコメント追加

--- sample/infra/users.py
+++ sample/infra/users.py
@@ -1,7 +1,18 @@
 from sample.models import User
+from aldjemy.orm import BaseSQLAModel
+
+# SQLAlchemyを使ってデータを取得
 
 
 def get_users() -> list:
+    # Userが、DjangoのModel
+    # User_sa=User.saが、SQLAlchemyのModel
+    User_sa: BaseSQLAModel = User.sa
+
+    # SQLAlchemyでUserの全てgetする : User_sa.query().all()
+    # -> 配列の各要素からdict形式でデータを取り出す : u.__dict__.items()
+    # -> dictからSQLAlchemyが付与する一時情報を削除 : k != "_sa_instance_state"
+    # いわゆる内包表記で書いています。Python特有の書き方なのでなれないと分かりにくいかもです。
     users = [{k: v for k, v in u.__dict__.items() if k != "_sa_instance_state"}
-             for u in User.sa.query().all()]
+             for u in User_sa.query().all()]
     return users
