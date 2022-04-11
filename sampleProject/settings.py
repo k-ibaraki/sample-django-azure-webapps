157b3f59cff8a7bd043d8be365ca888b079f27ed
k-ibaraki <ibaraki@ncdc.co.jp>
2022-04-11 19:04:36 +0900

    不要なenvを削除

--- sampleProject/settings.py
+++ sampleProject/settings.py
@@ -30,8 +30,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-CSRF_TRUSTED_ORIGINS = os.getenv(
-    'CSRF_TRUSTED_ORIGINS', '.azurewebsites.net').split(',')
 
 # Application definition
 
