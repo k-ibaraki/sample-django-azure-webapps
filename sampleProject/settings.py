4a53b6a48d415483b8c39cd0ace058f3d6df3357
k-ibaraki <ibaraki@ncdc.co.jp>
2022-04-11 17:12:37 +0900

    csrf settings

--- sampleProject/settings.py
+++ sampleProject/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    'CSRF_TRUSTED_ORIGINS', '.azurewebsites.net').split(',')
 
 # Application definition
 
