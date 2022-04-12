c9d054f13c34ff89fc7b870f04a8352b43429560
k-ibaraki <ibaraki@ncdc.co.jp>
2022-04-12 10:55:12 +0900

    不要なimportを削除

--- sampleProject/settings.py
+++ sampleProject/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import ssl
 from pathlib import Path
 import os
 from dotenv import load_dotenv
