cfbd7a13004c355fb4db1ad4b0b5c0150af4fb61
k-ibaraki <ibaraki@ncdc.co.jp>
2022-05-04 12:03:11 +0900

    add aws-xray

--- sampleProject/settings.py
+++ sampleProject/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # aws x-ray setting
+    'aws_xray_sdk.ext.django.middleware.XRayMiddleware',
 ]
 
 ROOT_URLCONF = 'sampleProject.urls'
